3d7f8b2fac5635953514cca5460e2c737dbf07e1
YuCheng <69775014+Yu-C-UTS@users.noreply.github.com>
2022-04-14 16:31:11 +0800

    Basic Damage Functionality

--- AbyssalsCall/Assets/Scripts/PlayerSubmarine.cs
+++ AbyssalsCall/Assets/Scripts/PlayerSubmarine.cs
@@ -13,14 +13,13 @@ public class PlayerSubmarine : MonoBehaviour
     Transform turretPivot;
 
     bool facingRight = true;
-    const float spriteFlipSpeed = 8;
 
     public UnityEvent onSubUpdate;
     public UnityEvent onSubFixedUpdate;
     public UnityEvent<Vector2> onMouseMove;
     public UnityEvent<Vector2> onMove;
-    public UnityEvent<float> onTriggerPrim;
-    public UnityEvent<float> onTriggerSec;
+    public UnityEvent<float, GameObject> onTriggerPrim;
+    public UnityEvent<float, GameObject> onTriggerSec;
 
     private void Awake() 
     {
@@ -33,7 +32,7 @@ public class PlayerSubmarine : MonoBehaviour
     {
         onSubUpdate?.Invoke();
 
-        UpdateSubmarineFacing();
+        //UpdateSubmarineFacing();
     }
 
     private void FixedUpdate() 
@@ -44,16 +43,18 @@ public class PlayerSubmarine : MonoBehaviour
     public void OnMoveInput(InputAction.CallbackContext value)
     {
         onMove?.Invoke(value.ReadValue<Vector2>());
+
+        UpdateSubmarineFacing(value.ReadValue<Vector2>().x);
     }
 
     public void OnTriggerPrimInput(InputAction.CallbackContext value)
     {
-        onTriggerPrim?.Invoke(value.ReadValue<float>());
+        onTriggerPrim?.Invoke(value.ReadValue<float>(), gameObject);
     }
 
     public void OnTriggerSecInput(InputAction.CallbackContext value)
     {
-        onTriggerSec?.Invoke(value.ReadValue<float>());
+        onTriggerSec?.Invoke(value.ReadValue<float>(), gameObject);
     }
 
     public void OnCursorMoveInput(InputAction.CallbackContext value)
@@ -61,7 +62,10 @@ public class PlayerSubmarine : MonoBehaviour
         onMouseMove?.Invoke(value.ReadValue<Vector2>());
     }
 
+    /*
+    const float spriteFlipSpeed = 8;
 
+    [System.Obsolete("Flip turn animation, this one uses current velocity to determine direction.")]
     private void UpdateSubmarineFacing()
     {
         if(!Mathf.Approximately(rigidBody2d.velocity.x, 0))
@@ -75,5 +79,16 @@ public class PlayerSubmarine : MonoBehaviour
         }
 
         transform.localScale = new Vector3(Mathf.Clamp(transform.localScale.x + ( facingRight ? 1 : -1 ) * Time.deltaTime * spriteFlipSpeed, -1, 1), 1, 1);
+    }
+    */
+
+    private void UpdateSubmarineFacing(float XMovementSpeed)
+    {
+        if(!Mathf.Approximately(XMovementSpeed, 0))
+        {
+            facingRight = XMovementSpeed > 0;
+        }
+
+        transform.localScale = new Vector3((facingRight ? 1 : -1), 1, 1);
     }
 }
---
+++ AbyssalsCall/Assets/Scripts/Projectiles/ProjectileBase.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class ProjectileBase : MonoBehaviour
+{
+    protected Collider2D col2d;
+    protected Rigidbody2D rb2d;
+
+    [SerializeField]
+    protected float projectileLifetime = 3f;
+
+    [SerializeField]
+    protected List<Payload> payloads;
+
+    protected virtual void Awake() 
+    {
+        col2d = GetComponent<Collider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public virtual void InitiateProjectile(GameObject projectileSource)
+    {
+        Destroy(gameObject, projectileLifetime);
+
+        Collider2D sourceCollider = projectileSource.GetComponent<Collider2D>();
+        if(sourceCollider != null)
+        {
+            Physics2D.IgnoreCollision(col2d, sourceCollider, true);
+            Debug.Log("Collider Ignoring Source");
+            StartCoroutine(RemoveColliderIgnore(sourceCollider, 0.5f));
+        }
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.TryGetComponent<IDamagable>(out IDamagable otherDamagable))
+        {
+            triggerPayload(otherDamagable, other.gameObject);
+        }
+    }
+
+    protected virtual void triggerPayload(IDamagable targetDamagable, GameObject targetGameobject)
+    {
+        foreach(Payload payload in payloads)
+        {
+            payload.ActivatePayload(targetDamagable, targetGameobject);
+        }
+    }
+
+    protected IEnumerator RemoveColliderIgnore(Collider2D targetCollider, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Physics2D.IgnoreCollision(col2d, targetCollider, false);
+        Debug.Log("Collider Reset");
+    }
+
+    protected virtual void OnValidate() 
+    {
+        
+    }
+}
