1525397eea32f711558432d1c63783e6e03fdc61
Zavier-SE <zavier.se@gmail.com>
2022-05-12 05:21:00 +0800

    Update enemy attacks
    Now all enemies can deal damage to the player

--- AbyssalsCall/Assets/Scripts/Creatures/Enemies/BasicEnemyController.cs
+++ AbyssalsCall/Assets/Scripts/Creatures/Enemies/BasicEnemyController.cs
@@ -8,9 +8,16 @@ public class BasicEnemyController : BasicEnemyMovement, IDamagable
     protected float maxHealth = 40;
     protected float Health;
 
+    public float AttackTime;
+    protected float AttackTimer;
+    protected bool canAttack;
+    protected Damage basicDamage = new Damage(5);
+
     protected override void Awake()
     {
         Health = maxHealth;
+        AttackTimer = AttackTime;
+        canAttack = true;
         pfController = new PathfindingController(transform, PickRandomPoint());
     }
 
@@ -20,9 +27,44 @@ public class BasicEnemyController : BasicEnemyMovement, IDamagable
         {
             Destroy(this.gameObject);
         }
+
+        if (!canAttack)
+        {
+            if(AttackCountDown() == 0)
+            {
+                canAttack = true;
+            }
+        }
+
     }
     public void TakeDamage(Damage damage)
     {
         Health -= damage.baseDamageValue;
+    }
+
+    protected virtual float AttackCountDown()
+    {
+        if (AttackTimer <= 0)
+        {
+            AttackTimer = AttackTime;
+            return 0;
+        }
+        else
+        {
+            AttackTimer -= Time.deltaTime;
+            return AttackTimer;
+        }
+    }
+
+    protected virtual void OnTriggerStay2D(Collider2D collision)
+    {
+        if (canAttack)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                collision.GetComponent<PlayerSubmarine>().TakeDamage(basicDamage);
+                canAttack = false;
+            }
+        }
     }
 }
